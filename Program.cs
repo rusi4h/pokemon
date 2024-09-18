d90418aafbdfc41eaf25cc896e614114a279a3ed
Rustam <125592752+rusi4h@users.noreply.github.com>
2024-09-18 19:22:50 +0400

    (R0) basic class
    abstract class with pokemon's parameters

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("R0");
+        }
+    }
+
+    public abstract class pokemon
+    {
+        public abstract void ShowHP();
+
+        public abstract void ShowDamage();
+
+        public abstract void ShowDefense();
+
+        public abstract void ShowSPAttack();
+
+        public abstract void ShowSPDefense();
+
+        public abstract void ShowSpeed();
+    }
+}
