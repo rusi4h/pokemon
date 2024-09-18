60e3462e886d6d01c633a88cf16c92b55ac8e8e5
Rustam <125592752+rusi4h@users.noreply.github.com>
2024-09-18 19:32:55 +0400

    (R1) Sandshrew create
    simple class with stats (without spec)

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace pokemon
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("R0");
+            Console.WriteLine("R1");
+            Console.WriteLine("---Sandshrew---");
         }
     }
 
@@ -27,5 +28,39 @@ namespace pokemon
         public abstract void ShowSPDefense();
 
         public abstract void ShowSpeed();
+    }
+
+    public class Sandshrew : pokemon
+    {
+        double HP = 50;
+        double Attack = 75;
+        double Defense = 85;
+        double SAttack = 20;
+        double SDefense = 30;
+        double Speed = 40;
+        public override void ShowHP()
+        {
+            Console.WriteLine("Здоровье = " + HP);
+        }
+        public override void ShowDamage()
+        {
+            Console.WriteLine("Атака = " + Attack);
+        }
+        public override void ShowDefense()
+        {
+            Console.WriteLine("Защита = " + Defense);
+        }
+        public override void ShowSPAttack()
+        {
+            Console.WriteLine("Спец. атака = " + SAttack);
+        }
+        public override void ShowSPDefense()
+        {
+            Console.WriteLine("Спец. защита = " + SDefense);
+        }
+        public override void ShowSpeed()
+        {
+            Console.WriteLine("Скорость = " + Speed);
+        }
     }
 }
